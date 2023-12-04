a1695b9f4166759a04d0cf86efdd22cfe528d323
Vitaliya Ovechko <ovechko.vita@gmail.com>
2023-12-04 21:10:28 +0200

    add readme file with screenshots

---
+++ README.md
@@ -0,0 +1,6 @@
+Screenshots
+
+1. https://monosnap.com/file/VxkEG2S1vinkKwpriPXPjQJXkUPOIr
+2. https://monosnap.com/file/HAYvP2jT5I9cO1Z2PGeRHcz9CFeis4
+3. https://monosnap.com/file/vUIcKH5T308CHlS8zLI2rEVxDsPtKW
+4. https://monosnap.com/file/mn4AbbjbrpZLCHQeuRvly6OJZbG0iJ
---
+++ contacts.js
@@ -0,0 +1,54 @@
+const fs = require("fs").promises;
+const path = require("path");
+const { nanoid } = require("nanoid");
+
+const contactsPath = path.join(__dirname, "db/contacts.json");
+
+// TODO: задокументувати кожну функцію
+const listContacts = async () => {
+  // ...твій код. Повертає масив контактів.
+  const data = await fs.readFile(contactsPath, "utf-8");
+  return JSON.parse(data);
+};
+
+const getContactById = async (contactId) => {
+  // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+  const contacts = await listContacts();
+  const result = contacts.find((contact) => contact.id === contactId);
+
+  return result || null;
+};
+
+const removeContact = async (contactId) => {
+  // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+  const contacts = await listContacts();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+
+  if (index === -1) {
+    return null;
+  }
+
+  const [result] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
+  return result;
+};
+
+const addContact = async (contact) => {
+  // ...твій код. Повертає об'єкт доданого контакту.
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    ...contact,
+  };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
